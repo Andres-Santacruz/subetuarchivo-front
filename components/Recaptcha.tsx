1f075acac99302ce2758cb9be2c172a87f498839
andres santacruz <andres_v10@live.com>
2022-12-09 06:21:58 -0500

    [ADD]: Add own recaptcha component

---
+++ components/Recaptcha.tsx
@@ -0,0 +1,35 @@
+import React, { Dispatch, SetStateAction, forwardRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+
+interface IPropsRecaptcha {
+  setErrorCaptcha: Dispatch<SetStateAction<boolean>>;
+  isInVisble?: string;
+}
+
+const Recaptcha = forwardRef<ReCAPTCHA, IPropsRecaptcha>(
+  ({ setErrorCaptcha, isInVisble }, ref) => {
+
+    const onChangeRecaptcha = (token: string | null) => {
+      console.log('Boolean(token)', Boolean(token))
+      setErrorCaptcha(!Boolean(token));
+    };    
+
+    return (
+      <>
+        <ReCAPTCHA
+          ref={ref}
+          sitekey={
+            process.env.NEXT_PUBLIC_RECAPTCHA_KEY ||
+            "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+          }
+          onChange={onChangeRecaptcha}
+          size={isInVisble ? "invisible" : undefined}
+        />
+      </>
+    );
+  }
+);
+
+Recaptcha.displayName = "Recaptcha";
+
+export default Recaptcha;
