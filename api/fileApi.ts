d93153aa6c15e83847bdb1c1bbd7546e145239ce
andres santacruz <andres_v10@live.com>
2023-01-18 15:35:46 -0500

    [BUILD]: Change endpoint

--- api/fileApi.ts
+++ api/fileApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseURL = "http://localhost:3001/api"
-const baseURL = "https://subetuarchivo.up.railway.app/api";
+const baseURL = "https://subetuarchivo.onrender.com/api";
 
 const axiosApi = axios.create({ baseURL });
 
